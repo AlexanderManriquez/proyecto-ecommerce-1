d80446943d451b142ca7e36e864866ef2b04aa8b
AlexanderManriquez <alexander.manriquez.s@gmail.com>
2025-03-22 16:21:19 -0300

    Refactor Categories component to use CategoryCard for better code reuse and readability; enhance accessibility in Navbar and Hero components with appropriate aria-labels.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,25 +2,25 @@ export default function Navbar() {
     return (
         <nav className="flex justify-between items-center p-4 bg-gray-800">
             <div className="flex">
-                <a href="#" target="_blank">
+                <a href="#" aria-label="Ir a la página principal">
                     <i className="fa-brands fa-web-awesome text-amber-400"></i>
                 </a>
                 <p className="ml-2">La Tienda</p>
             </div>
             <ul className="w-1/4 flex justify-around">
-                <li><a className="" href="#" target="_blank">Mujer</a></li>
-                <li><a className="" href="#" target="_blank">Hombre</a></li>
-                <li><a className="" href="#" target="_blank">Niños</a></li>
+                <li><a className="" href="#" aria-label="Ver sección de Mujer">Mujer</a></li>
+                <li><a className="" href="#" aria-label="Ver sección de Hombre">Hombre</a></li>
+                <li><a className="" href="#" aria-label="Ver sección de Niños">Niños</a></li>
             </ul>
             <ul className="w-1/4 flex justify-around">
                 <li>
-                    <a className="" href="/login.html" target="_blank">Iniciar sesión</a>
+                    <a className="" href="/login.html" aria-label="Iniciar sesión">Iniciar sesión</a>
                 </li>
                 <li>
-                    <a className="" href="/signup.html" target="_blank">Registrarse</a>
+                    <a className="" href="/signup.html" aria-label="Registrarse">Registrarse</a>
                 </li>
                 <li>
-                    <a className="" href="/cart.html" aria-label="Abrir carrito de compras" target="_blank">
+                    <a className="" href="/cart.html" aria-label="Abrir carrito de compras">
                         <i className="fa-solid fa-cart-shopping"></i>
                     </a>
                 </li>
